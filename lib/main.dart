45e394beb1ea68a5c6fbcd760db0b63d583eb72f
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-09-08 23:27:47 +0300

    add media query

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MediaQueryTest());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MediaQueryTest extends StatelessWidget {
+
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var height= MediaQuery.of (context).size.height;
     return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.black,
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.red,
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.pink,
+            ),
+          ],
+        ),
+      ),
 
 
     );
